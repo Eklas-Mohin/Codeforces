98629bb31278bd3b61a3fe1ead3cdc375f552fb9
Eklas-Mohin <mohin.030299@gmail.com>
2024-02-04 00:10:42 +0600

    151A

---
+++ soft_drinking.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n, k, l, c, d, p, nl, np, toasts;
+    cin >> n >> k >> l >> c >> d >> p >> nl >> np;
+    l = l * k;
+    c = c * d;
+    toasts = l / nl; 
+    toasts = min(toasts, c);
+    toasts = min(toasts, p / np);
+    toasts /= n;
+    cout<< toasts <<endl;
+	return 0;
+}
