2ee11cedc962461190fa4987de13ed40432cf17e
Eklas-Mohin <mohin.030299@gmail.com>
2024-02-08 21:36:57 +0600

    Codeforces 231A

--- team.cpp
+++ team.cpp
@@ -1,14 +1,19 @@
-#include <iostream>
-#include <cmath>
+/**
+ *    author: mohin    
+**/
+
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
     int n{}, a{}, b{}, c{}, ans{};
     cin >> n;
     while (n--) {
         cin >> a >> b >> c;
-        if ((a + b + c) >= 2) {
+        if ((a + b + c) > 1) {
             ans += 1;
         }
     }
