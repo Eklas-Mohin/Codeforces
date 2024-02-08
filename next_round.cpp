7c7e6256dd7b6d12098bafd6dc194555f29e25c8
Eklas-Mohin <mohin.030299@gmail.com>
2024-02-08 21:38:12 +0600

    Codeforces 158A

--- next_round.cpp
+++ next_round.cpp
@@ -1,9 +1,14 @@
-#include <iostream>
-#include <cmath>
+/**
+ *    author: mohin    
+**/
+
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
     int n{}, k{}, ans{};
     int arr[51] = {};
     cin >> n >> k;
