33d6f12893e7ccfb351f3170778651fb1badaf7f
Eklas-Mohin <mohin.030299@gmail.com>
2024-02-09 10:32:28 +0600

    Codeforces 116A

---
+++ tram.cpp
@@ -0,0 +1,23 @@
+/**
+ *    author: mohin    
+**/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n{}, a{}, b{}, ans{}, count{};
+    cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        cin >> a >> b;
+        count -= a;
+        count += b;
+        ans = max(ans, count);
+    }
+    cout << ans << endl;
+    return 0;
+}
+ 
