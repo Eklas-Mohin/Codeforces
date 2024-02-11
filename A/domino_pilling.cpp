6af938cda550af12ab25eb73a9f99d24c5d01361
Eklas-Mohin <mohin.030299@gmail.com>
2024-02-11 14:48:44 +0600

    Codeforces 50A

---
+++ A/domino_pilling.cpp
@@ -0,0 +1,16 @@
+/**
+ *    author: mohin    
+**/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int m{}, n{};
+    cin >> m >> n;
+    cout << (m * n) / 2 << endl;
+    return 0;
+}
