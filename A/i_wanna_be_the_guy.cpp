40ec46c9ecc828ff4f69ff702a286e276f86040f
Eklas-Mohin <mohin.030299@gmail.com>
2024-02-11 14:26:49 +0600

    Codeforcs 469A

---
+++ A/i_wanna_be_the_guy.cpp
@@ -0,0 +1,31 @@
+/**
+ *    author: mohin    
+**/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n{}, x{}, y{}, in{}, arr[101] = {};
+    cin >> n >> x;
+    for (int i = 0; i < x; ++i) {
+        cin >> in;
+        arr[in] = 1;
+    }
+    cin >> y;
+    for (int i = 0; i < y; ++i) {
+        cin >> in;
+        arr[in] = 1;
+    }
+    for (int i = 1; i <= n; ++i) {
+        if (arr[i] != 1) {
+            cout << "Oh, my keyboard!" << endl;
+            return 0;
+        }
+    }
+    cout << "I become the guy." << endl;
+    return 0;
+}
