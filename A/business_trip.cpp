9ce1522be7c26240a6d0bc0946b4f00aa3d2584a
Eklas-Mohin <mohin.030299@gmail.com>
2024-03-08 16:00:52 +0600

    Codeforces 149A

---
+++ A/business_trip.cpp
@@ -0,0 +1,31 @@
+/**
+ *    author: mohin    
+**/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int k{}, ans{}, arr[12] = {};
+    cin >> k;
+    for (int i = 0; i < 12; ++i) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + 12, greater<int>());
+    for (int i = 0; i < 12; ++i) {
+        if (k < 1) {
+            break;
+        } else {
+            k = k - arr[i];
+            ans += 1;
+        }
+    }
+    if (k > 0) {
+        ans = - 1;
+    }
+    cout << ans << endl;
+    return 0; 
+}
