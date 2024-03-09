f120ce6f02c890de156f5ebafd3471e2567843c4
Eklas-Mohin <mohin.030299@gmail.com>
2024-03-09 13:04:47 +0600

    Codeforces 221A

---
+++ A/little_elephant_and_function.cpp
@@ -0,0 +1,20 @@
+/**
+ *    author: mohin    
+**/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n{};
+    cin >> n;
+    cout << n << ' ';
+    for (int i = 1; i < n; ++i) {
+        cout << i << ' ';
+    }
+    cout << endl;
+    return 0;
+}
