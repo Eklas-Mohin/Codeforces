7f4278039f30cb8ce5fa08aeda323fa01c74ce23
Eklas-Mohin <mohin030299@gmail.com>
2024-07-03 06:37:10 +0600

    Codeforces 1857A

---
+++ A/array_coloring.cpp
@@ -0,0 +1,22 @@
+/**
+ *    author: mohin    
+**/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t{}, n{}, arr[50];
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        for (int i = 0; i < n; ++i) {
+            cin >> arr[i];
+        }
+        cout << "YES" << endl;
+    }
+    return 0;
+}
