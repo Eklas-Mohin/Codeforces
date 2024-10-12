3fe5619358a4675bbd8467d1ac6b5ebf9fad111a
Eklas-Mohin <mohin030299@gmail.com>
2024-10-12 11:02:45 +0600

    Codeforces 1374B

---
+++ B/multiply2_divide6.py
@@ -0,0 +1,26 @@
+''' author: mohin
+    problrm: Codeforces 1374B
+'''
+
+def main():
+    t = int(input())
+    
+    for _ in range(t):
+        n = int(input())
+        two = three = 0
+        
+        while n % 2 == 0:
+            two += 1
+            n //= 2
+            
+        while n % 3 == 0:
+            three += 1
+            n //= 3
+            
+        if n != 1 or two > three:
+            print(-1)
+        else:
+            print(2 * three - two)
+            
+if __name__ == '__main__':
+    main()
