a9cee5e8f476506495b66624318555f282e5c4e7
Eklas-Mohin <mohin030299@gmail.com>
2024-10-13 21:40:57 +0600

    Codeforces 4A Solution in Java

--- A/Watermelon.java
+++ A/Watermelon.java
@@ -7,18 +7,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 public class Watermelon {
 
+    public static boolean isPossible(int w) {
+        return w > 2 && w % 2 == 0;
+    }
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter writer = new PrintWriter(System.out);
 
-        int w = Integer.parseInt(br.readLine());
+        int watermelonWeight = Integer.parseInt(reader.readLine());
 
-        if (w > 2 && w % 2 == 0) {
-            System.out.println("YES");
+        if (isPossible(watermelonWeight)) {
+            writer.println("YES");
         } else {
-            System.out.println("NO");
+            writer.println("NO");
         }
+
+        writer.flush();
     }
 }
