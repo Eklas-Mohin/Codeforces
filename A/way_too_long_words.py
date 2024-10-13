0e4733a993ee2e775d8e355677a58508b7f05725
Eklas-Mohin <mohin030299@gmail.com>
2024-10-14 02:10:59 +0600

    Codeforces 71A Solution in Python

--- A/way_too_long_words.py
+++ A/way_too_long_words.py
@@ -6,14 +6,12 @@
 
 def solve(s: str) -> str:
     length = len(s)
-    
     if length > 10:
         return f"{s[0]}{length - 2}{s[-1]}"
     return s
 
 def main():
     n = int(input())
-    
     for _ in range(n):
         s = input()
         print(solve(s))
