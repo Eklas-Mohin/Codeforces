44d403f7c90d644cacaa42ed16a8dec9fd4bccd4
Eklas-Mohin <mohin030299@gmail.com>
2024-10-13 19:33:49 +0600

    Codeforces 4A Solution in Java

--- A/Watermelon.java
+++ A/Watermelon.java
@@ -1,26 +1,25 @@
-/*****************************
- *   author:  mohin          *
- *   problem: Codeforces 4A  *
- *   verdict: Accepted       *
- *****************************/
 
-import java.lang.*;
-import java.io.*;
-import java.util.*;
+/******************************
+ *   author:  mohin           *
+ *   problem: Codeforces 4A   *
+ *   verdict: Accepted        *
+ ******************************/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Watermelon {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-        
+
         int w = Integer.parseInt(br.readLine());
- 
+
         if (w > 2 && w % 2 == 0) {
-            out.println("YES");
+            System.out.println("YES");
         } else {
-            out.println("NO");
+            System.out.println("NO");
         }
-        
-        out.flush();
     }
 }
