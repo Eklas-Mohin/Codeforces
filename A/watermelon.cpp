98628d266a27f54fd395b7376da8e5278d3bad8d
Eklas-Mohin <mohin030299@gmail.com>
2024-10-13 16:22:06 +0600

    Codeforces 4A

--- A/watermelon.cpp
+++ A/watermelon.cpp
@@ -15,7 +15,7 @@ int main() {
     int w;
     cin >> w;
 
-    if (w > 2 && w % 2 == 0) {
+    if (w > 2 and w % 2 == 0) {
         cout << "YES" << endl;
     } else {
         cout << "NO" << endl;
