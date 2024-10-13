2a0c4addf0eb08f7dc0249392a5bff3ca2ffd5e2
Eklas-Mohin <mohin030299@gmail.com>
2024-10-13 20:14:20 +0600

    Codeforces 4A Solution in python

---
+++ A/way_too_long_words.py
@@ -0,0 +1,21 @@
+# *************************** #
+#   author:  mohin            #
+#   problem: Codeforces 71A   #
+#   verdict: Accepted         #
+# *************************** #
+
+def solve(s: str) -> str:
+    length = len(s)
+    if length > 10:
+        return f"{s[0]}{length - 2}{s[-1]}"
+    return s
+
+def main():
+    n = int(input())
+    for _ in range(n):
+        s = input()
+        print(solve(s))
+
+if __name__ == "__main__":
+    main()
+
