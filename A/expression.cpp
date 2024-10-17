e42b514d89dbe5da7a6068956e6d7d6b7addaf5c
Eklas-Mohin <mohin030299@gmail.com>
2024-10-18 03:12:14 +0600

    Codeforces 479A Solution in C++

--- A/expression.cpp
+++ A/expression.cpp
@@ -1,20 +1,73 @@
-/**
- *    author: mohin    
-**/
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <array>
+#include <list>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <utility>
+#include <functional>
+#include <limits>
+#include <random>
+#include <ctime>
+#include <chrono>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <regex>
+#include <concepts>
+#include <ranges>
+#include <type_traits>
+#include <exception>
+#include <stdexcept>
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <cfloat>
+
+#define MAX_SIZE 1000005
+#define MOD 1000000007
+#define INF INT_MAX
+#define EPS 1e-9
+#define PI acos(-1)
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
+int maxValue(int a, int b, int c) {
+    int max_val = max(a * b * c, a + b + c);
+    max_val = max(max_val, a + (b * c));
+    max_val = max(max_val, a * (b + c));
+    max_val = max(max_val, (a + b) * c);
+    return max_val;
+}
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int a{}, b{}, c{}, ans{};
-    cin >> a >> b >> c;
-    ans = a + b + c;
-    ans = max(ans, a * b * c);
-    ans = max(ans, (a + b) * c);
-    ans = max(ans, a * (b + c));
-    cout << ans << endl;
-    return 0; 
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int a, b, c;
+    cin >> a >> c;
+    cout << maxValue(a, b, c) << endl;
+
+    return 0;
 }
