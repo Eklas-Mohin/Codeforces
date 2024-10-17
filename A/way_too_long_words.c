4202d4ab1a7f3f31c786de6c6f31d705d72e4818
Eklas-Mohin <mohin030299@gmail.com>
2024-10-17 18:13:22 +0600

    Codeforces 71A Solution in C

--- A/way_too_long_words.c
+++ A/way_too_long_words.c
@@ -1,11 +1,37 @@
-/*****************************
- *   author:  mohin          *
- *   problem: Codeforces 71A *
- *   verdict: Accepted       *
- *****************************/
-
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <limits.h>
+#include <float.h>
+#include <time.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <errno.h>
+#include <assert.h>
+#include <signal.h>
+#include <setjmp.h>
+#include <stdarg.h>
+#include <locale.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+#define MAX_SIZE 1000005
+#define INF INT_MAX
+#define MOD 1000000007
+#define PI acos(-1)
+
+#ifdef DEBUG
+#define debug(fmt, ...) fprintf(stderr, fmt, ##__VA_ARGS__)
+#else
+#define debug(fmt, ...)
+#endif
 
 void solve(char *str, char *res) {
     int len = strlen(str);
