458312e92bf1a5c3793b94eb0bab3ba0ef6f9ecc
Eklas-Mohin <mohin030299@gmail.com>
2024-10-17 17:46:07 +0600

    Codeforces 4A Solution in C

--- A/watermelon.c
+++ A/watermelon.c
@@ -1,20 +1,45 @@
-/*****************************
- *   author:  mohin          *
- *   problem: Codeforces 4A  *
- *   verdict: Accepted       *
- *****************************/
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <limits.h>
+#include <float.h>
+#include <time.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <errno.h>
+#include <assert.h>
+#include <signal.h>
+#include <setjmp.h>
+#include <stdarg.h>
+#include <locale.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+#define MAX_SIZE 100005
+#define INF INT_MAX
+#define MOD 1000000007
+#define PI acos(-1)
+
+#ifdef DEBUG
+#define debug(fmt, ...) fprintf(stderr, fmt, ##__VA_ARGS__)
+#else
+#define debug(fmt, ...)
+#endif
 
 int main() {
     int w;
     scanf("%d", &w);
-
-    if (w > 2 && w % 2 == 0) {
+    if (w % 2 == 0 && w > 2) {
         printf("YES\n");
     } else {
         printf("NO\n");
     }
-
     return 0;
 }
