9e26755e7e93111bd3fa9ada57329195a80d1a65
Eklas-Mohin <mohin030299@gmail.com>
2024-10-17 22:22:40 +0600

    Codeforces 977A Solution in C++

--- A/wrong_subtraction.cpp
+++ A/wrong_subtraction.cpp
@@ -1,16 +1,66 @@
-/**
- *    author: mohin    
-**/
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <array>
+#include <list>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <utility>
+#include <functional>
+#include <limits>
+#include <random>
+#include <ctime>
+#include <chrono>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <regex>
+#include <concepts>
+#include <ranges>
+#include <type_traits>
+#include <exception>
+#include <stdexcept>
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <cfloat>
+
+#define MAX_SIZE 1000005
+#define MOD 1000000007
+#define INF INT_MAX
+#define EPS 1e-9
+#define PI acos(-1)
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int n{}, k{};
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int n, k;
+    
     cin >> n >> k;
+
     while (k--) {
         if (n % 10 == 0) {
             n /= 10;
@@ -18,6 +68,8 @@ int main() {
             n -= 1;
         }
     }
+
     cout << n << endl;
+
     return 0;
 }
