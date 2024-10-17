19a1790d2e8c818c5766877604bf2e46c504ed46
Eklas-Mohin <mohin030299@gmail.com>
2024-10-18 00:43:23 +0600

    Codeforces 281A Solution in C++

--- A/word_capitalization.cpp
+++ A/word_capitalization.cpp
@@ -1,19 +1,71 @@
-/**
- *    author: mohin    
-**/
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <array>
+#include <list>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <utility>
+#include <functional>
+#include <limits>
+#include <random>
+#include <ctime>
+#include <chrono>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <regex>
+#include <concepts>
+#include <ranges>
+#include <type_traits>
+#include <exception>
+#include <stdexcept>
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <cfloat>
+
+#define MAX_SIZE 1000005
+#define MOD 1000000007
+#define INF INT_MAX
+#define EPS 1e-9
+#define PI acos(-1)
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    string str{};
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    string str;
+
     cin >> str;
+
     if (str[0] >= 97) {
         str[0] = str[0] - 32;
     }
+
     cout << str << endl;
+
     return 0;
 }
