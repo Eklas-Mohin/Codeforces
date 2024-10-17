ed672825cc82ebb7d90bdab0905e23c43b3f1c48
Eklas-Mohin <mohin030299@gmail.com>
2024-10-17 23:05:20 +0600

    Codeforces 59A Solution in C++

--- A/word.cpp
+++ A/word.cpp
@@ -1,17 +1,67 @@
-/**
- *    author: mohin    
-**/
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <array>
+#include <list>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+#include <iterator>
+#include <utility>
+#include <functional>
+#include <limits>
+#include <random>
+#include <ctime>
+#include <chrono>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <regex>
+#include <concepts>
+#include <ranges>
+#include <type_traits>
+#include <exception>
+#include <stdexcept>
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <cfloat>
+
+#define MAX_SIZE 1000005
+#define MOD 1000000007
+#define INF INT_MAX
+#define EPS 1e-9
+#define PI acos(-1)
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    string str{};
-    int lower{}, upper{};
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    string str;
+    int lower = 0, upper = 0;
+
     cin >> str;
+
     for (int i = 0; i < str.length(); ++i) {
         if (str[i] >= 97) {
             lower += 1;
@@ -19,11 +69,14 @@ int main() {
             upper += 1;
         }
     }
+
     if (upper > lower) {
         transform(str.begin(), str.end(), str.begin(), ::toupper); 
     } else {
         transform(str.begin(), str.end(), str.begin(), ::tolower);
     }
+
     cout << str << endl;
+
     return 0;
 }
