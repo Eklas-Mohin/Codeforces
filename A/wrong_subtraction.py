de4ed1393f89d6b99bac7270fb5e14ad1a405948
Eklas-Mohin <mohin030299@gmail.com>
2024-10-18 16:16:41 +0600

    Codeforces 977A Solution in Python

--- A/wrong_subtraction.py
+++ A/wrong_subtraction.py
@@ -1,11 +1,13 @@
-def wrong_subtraction(n, k):
+def main():
+    n, k = map(int, input().split())
+    
     for _ in range(k):
         if n % 10 == 0:
             n //= 10
         else:
-            n -= 1
-    return n
+            n = n - 1
 
-if __name__ == '__main__':
-    n, k = map(int, input().split())
-    print(wrong_subtraction(n, k))
+    print(n)
+    
+if __name__ == "__main__":
+    main()
