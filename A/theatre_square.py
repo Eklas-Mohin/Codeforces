a3e87934c2c7d30b159d10d1c7acf9f9e2841cfd
Eklas-Mohin <mohin030299@gmail.com>
2024-10-18 16:21:26 +0600

    Codeforces 1A Solution in Python

--- A/theatre_square.py
+++ A/theatre_square.py
@@ -1,15 +1,10 @@
-# *************************** #
-#   author:  mohin            #
-#   problem: Codeforces 1A    #
-#   verdict: Accepted         #
-# *************************** #
-
 def main():
     n, m, a = map(int, input().split())
+    
     r = (n + a - 1) // a
     c = (m + a - 1) // a
     
     print(r * c)
     
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
