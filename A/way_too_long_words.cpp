9af5d0a0ecdaef91d7586664f55ed74b21cd937b
Eklas-Mohin <mohin030299@gmail.com>
2024-10-21 21:37:31 +0600

    Codeforces 71A. Way Too Long Words Solution in C++

--- A/way_too_long_words.cpp
+++ A/way_too_long_words.cpp
@@ -1,49 +1,5 @@
 #include <iostream>
-#include <iomanip>
-#include <sstream>
-#include <fstream>
 #include <string>
-#include <cstring>
-#include <vector>
-#include <array>
-#include <list>
-#include <deque>
-#include <set>
-#include <unordered_set>
-#include <map>
-#include <unordered_map>
-#include <queue>
-#include <stack>
-#include <algorithm>
-#include <numeric>
-#include <iterator>
-#include <utility>
-#include <functional>
-#include <limits>
-#include <random>
-#include <ctime>
-#include <chrono>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <regex>
-#include <concepts>
-#include <ranges>
-#include <type_traits>
-#include <exception>
-#include <stdexcept>
-#include <cassert>
-#include <cstddef>
-#include <cstdint>
-#include <cfloat>
-
-#define MAX_SIZE 100005
-#define MOD 1000000007
-#define INF INT_MAX
-#define EPS 1e-9
-#define PI acos(-1)
-
-#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -71,6 +27,7 @@ int main() {
     string str;
 
     cin >> n;
+
     while (n--) {
         cin >> str;
         cout << solve(str) << endl;
