de7ff350913a50d852803474b6e31b3e5415c8d8
Eklas-Mohin <mohin030299@gmail.com>
2024-10-21 21:49:14 +0600

    Codeforces 231A. Team Solution in C++

--- A/team.cpp
+++ A/team.cpp
@@ -1,22 +1,28 @@
-/**
- *    author: mohin    
-**/
-
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int n{}, a{}, b{}, c{}, ans{};
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int n, a, b, c, ans = 0;
     cin >> n;
+    
     while (n--) {
         cin >> a >> b >> c;
         if ((a + b + c) > 1) {
             ans += 1;
         }
     }
+
     cout << ans << endl;
+
     return 0;
 }
