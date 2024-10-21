92ed19bb850fe34f0c28276b513828d179ed55c7
Eklas-Mohin <mohin030299@gmail.com>
2024-10-21 21:33:44 +0600

    Codeforces 71A. Way Too Long Words Solution in C

--- A/way_too_long_words.c
+++ A/way_too_long_words.c
@@ -1,37 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include <ctype.h>
-#include <limits.h>
-#include <float.h>
-#include <time.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <errno.h>
-#include <assert.h>
-#include <signal.h>
-#include <setjmp.h>
-#include <stdarg.h>
-#include <locale.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/time.h>
-#include <pthread.h>
-
-#define MAX_SIZE 1000005
-#define INF INT_MAX
-#define MOD 1000000007
-#define PI acos(-1)
-
-#ifdef DEBUG
-#define debug(fmt, ...) fprintf(stderr, fmt, ##__VA_ARGS__)
-#else
-#define debug(fmt, ...)
-#endif
+#include <cstring>
 
 void solve(char *str, char *res) {
     int len = strlen(str);
@@ -46,7 +14,7 @@ void solve(char *str, char *res) {
 int main() {
     int n;
     char str[101], abbr[101];
-
+    
     scanf("%d", &n);
     
     while (n--) {
@@ -57,4 +25,3 @@ int main() {
 
     return 0;
 }
-
