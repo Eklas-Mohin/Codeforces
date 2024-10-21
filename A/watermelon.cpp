1eadfa88de66e300e07f1972923bfa60f88ac7d5
Eklas-Mohin <mohin030299@gmail.com>
2024-10-21 21:25:26 +0600

    Codeforces 4A. Watermelon Solution in C++

--- A/watermelon.cpp
+++ A/watermelon.cpp
@@ -1,47 +1,4 @@
 #include <iostream>
-#include <iomanip>
-#include <sstream>
-#include <fstream>
-#include <string>
-#include <cstring>
-#include <vector>
-#include <array>
-#include <list>
-#include <deque>
-#include <set>
-#include <unordered_set>
-#include <map>
-#include <unordered_map>
-#include <queue>
-#include <stack>
-#include <algorithm>
-#include <numeric>
-#include <iterator>
-#include <utility>
-#include <functional>
-#include <limits>
-#include <random>
-#include <ctime>
-#include <chrono>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <regex>
-#include <concepts>
-#include <ranges>
-#include <type_traits>
-#include <exception>
-#include <stdexcept>
-#include <cassert>
-#include <cstddef>
-#include <cstdint>
-#include <cfloat>
-
-#define MAX_SIZE 100005
-#define MOD 1000000007
-#define INF INT_MAX
-#define EPS 1e-9
-#define PI acos(-1)
 
 using namespace std;
 
@@ -56,7 +13,6 @@ int main() {
     #endif
 
     int w;
-    
     cin >> w;
     
     if (w % 2 == 0 and w > 2) {
